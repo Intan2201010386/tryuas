976aa19a24ebb3871fd952daa5335b098e16ff5b
Intan2201010386 <127836584+Intan2201010386@users.noreply.github.com>
2023-06-26 09:51:31 +0800

    Add files via upload

---
+++ uas-webpro-new/hitung.js
@@ -0,0 +1,32 @@
+const buttonHitungNilai = document.querySelector('.btnHitung');
+buttonHitungNilai.addEventListener("onclick", function(){
+    const nilaiTugas = document.querySelector('.nilaiTugas').value;
+    const nilaiUts = document.querySelector('.nilaiUts').value;
+    const nilaiUas = document.querySelector('.nilaiUas').value;
+
+    const nilaiAkhir = () => {
+        const nilaiAngka = document.querySelector('.nilaiAngka');
+        const hasil = (nilaiTugas * 20 / 100) + (nilaiUts * 30 / 100) + (nilaiUas * 50 / 100);
+
+        nilaiAngka.innerHTML ='<div class="box box-1"> <p class="box-header">Nilai Angka</p><div class="nilaiAngka"><h1>'+hasil+'</h1></div></div>';
+
+        const nilaiHuruf = document.querySelector('.nilaiHuruf');
+        let grade;
+        if(hasil > 80 <= 100){
+            grade = 'A';
+        } else if(hasil <= 80) {
+            grade = 'B';
+        } else if(hasil <= 60) {
+            grade = 'C';
+        } else if(hasil <= 50) {
+            grade = 'D';
+        } else {
+            grade = 'E';
+        }
+
+        nilaiHuruf.innerHTML ='<div class="box box-2"><p class="box-header">Nilai Huruf</p><div class="nilaiAngka"><h1>'+grade+'</h1></div>"</div>';
+    }
+    nilaiAkhir();
+
+    
+});
